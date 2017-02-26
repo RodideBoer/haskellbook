04dc07f508b12493c24c654fd733cda9d97b7bf7
Rodi de Boer <rodi@procurios.nl>
2017-02-26 15:24:37 +0100

    Make it interactive, a bit

--- chapter13/hello/exe/Main.hs
+++ chapter13/hello/exe/Main.hs
@@ -2,8 +2,12 @@ module Main where
 
 import Hello
 import DogsRule
+import System.IO
 
 main :: IO ()
 main = do
-  sayHello
-  dogs
+    hSetBuffering stdout NoBuffering
+    putStr "What is your name? "
+    name <- getLine
+    sayHello name
+    dogs
