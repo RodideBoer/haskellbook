2396fb26e5e6f1734ca7d52e043fa9cd4393aaff
Rodi de Boer <rodi@procurios.nl>
2017-03-07 08:39:38 +0100

    Add the hspec code from ch14

--- chapter14/addition/Addition.hs
+++ chapter14/addition/Addition.hs
@@ -1,6 +1,13 @@
 module Addition where
 
 import Test.Hspec
+import Test.QuickCheck
+
+prop_additionGreater :: Int -> Bool
+prop_additionGreater x = x + 1 > x
+
+runQc :: IO ()
+runQc = quickCheck prop_additionGreater
 
 main :: IO ()
 main = hspec $ do
@@ -9,6 +16,9 @@ main = hspec $ do
       ((1 :: Integer) + 1) > 1 `shouldBe` True
     it "2 + 2 should equal to 4" $ do
       (2 :: Integer) + 2 `shouldBe` 4
+    it "x + 1 is always greater than x" $ do
+      property $ \x -> x + 1 > (x :: Int)
+
     it "15 divided by 3 is 5" $ do
       (dividedBy 15 3 :: (Integer, Integer)) `shouldBe` (5, 0)
     it "22 divided by 5 is 4 remainder 2" $ do
@@ -37,3 +47,48 @@ multi x y
   | y == 0    = 0
   | y < 0     = -multi x (-y)
   | otherwise = x + multi x (y - 1)
+
+genBool :: Gen Bool
+genBool = choose (False, True)
+genBool' :: Gen Bool
+genBool' = elements [False, True]
+genBool'' :: Gen Bool
+genBool'' = elements [False, False, False, False, False, False, False, True]
+genOrdering :: Gen Ordering
+genOrdering = elements [LT, EQ, GT]
+genChar :: Gen Char
+genChar = elements ['a'..'z']
+
+genTuple :: (Arbitrary a, Arbitrary b) => Gen (a, b)
+genTuple = do
+  a <- arbitrary
+  b <- arbitrary
+  return (a, b)
+
+genThreeple :: (Arbitrary a, Arbitrary b, Arbitrary c) => Gen (a, b, c)
+genThreeple = do
+  a <- arbitrary
+  b <- arbitrary
+  c <- arbitrary
+  return (a, b, c)
+
+genEither :: (Arbitrary a, Arbitrary b) => Gen (Either a b)
+genEither = do
+  a <- arbitrary
+  b <- arbitrary
+  elements [Left a, Right b]
+
+-- equal probability
+genMaybe :: Arbitrary a => Gen (Maybe a)
+genMaybe = do
+  a <- arbitrary
+  elements [Nothing, Just a]
+
+-- What QuickCheck actually does
+-- so you get more Just values
+genMaybe' :: Arbitrary a => Gen (Maybe a)
+genMaybe' = do
+  a <- arbitrary
+  frequency [ (1, return Nothing)
+            , (3, return (Just a))]
+-- frequency :: [(Int, Gen a)] -> Gen a
